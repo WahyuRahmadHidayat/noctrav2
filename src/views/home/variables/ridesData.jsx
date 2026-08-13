dbd5169e1b0382659011e2a9c296a9cdecae552b
Wahyu <amadday09@gmail.com>
2026-08-13 11:52:38 +0700

    AutoRepo Push

---
+++ src/views/home/variables/ridesData.jsx
@@ -0,0 +1,28 @@
+const ridesData = [
+  { 
+    date: "16", 
+    month: "AUG", 
+    title: "CITY AFTER DARK", 
+    time: "20:00 WIB", 
+    loc: "Monas, Jakarta", 
+    img: "https://images.unsplash.com/photo-1555899434-94d1368aa7af?q=80&w=600&auto=format&fit=crop" 
+  },
+  { 
+    date: "23", 
+    month: "AUG", 
+    title: "NEON LOOP", 
+    time: "20:30 WIB", 
+    loc: "Bintaro Xchange", 
+    img: "https://images.unsplash.com/photo-1600320254374-ce2d293c324e?q=80&w=600&auto=format&fit=crop" 
+  },
+  { 
+    date: "30", 
+    month: "AUG", 
+    title: "MIDNIGHT 50", 
+    time: "21:00 WIB", 
+    loc: "GBK, Jakarta", 
+    img: "https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?q=80&w=600&auto=format&fit=crop" 
+  },
+];
+
+export default ridesData;
