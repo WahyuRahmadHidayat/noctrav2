2b4918eb9276d5a2caecc6ef0e7b504e3cb37d74
WahyuRahmadHidayat <[amadday09@gmail.com]>
2026-09-04 08:24:12 +0700

    feat: hapus dikit aja

--- src/views/ContactView.jsx
+++ src/views/ContactView.jsx
@@ -30,14 +30,14 @@ export default function ContactView() {
                 <div className="bg-primary/10 p-3 border border-primary/20 text-primary"><Mail size={24} /></div>
                 <div className="pt-1">
                   <h4 className="font-bold text-sm tracking-widest uppercase mb-2 text-white">Email</h4>
-                  <a href="mailto:crew@noctra.cc" className="text-gray-400 text-sm leading-relaxed hover:text-primary transition-colors block">crew@noctra.cc</a>
+                  <a href="mailto: amadday09@gmail.com" className="text-gray-400 text-sm leading-relaxed hover:text-primary transition-colors block">crew@noctra.cc</a>
                 </div>
               </div>
               <div className="flex items-start gap-4">
                 <div className="bg-primary/10 p-3 border border-primary/20 text-primary"><Phone size={24} /></div>
                 <div className="pt-1">
                   <h4 className="font-bold text-sm tracking-widest uppercase mb-2 text-white">Emergency / Hotline</h4>
-                  <a href="tel:+622155550123" className="text-gray-400 text-sm leading-relaxed hover:text-primary transition-colors block">+62 21 5555 0123</a>
+                  <a href="tel:+62 81528942073" className="text-gray-400 text-sm leading-relaxed hover:text-primary transition-colors block">+62 21 5555 0123</a>
                 </div>
               </div>
             </div>
